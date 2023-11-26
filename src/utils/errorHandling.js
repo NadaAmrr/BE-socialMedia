6350129be197c12d9ca1a2c8a0b458a9ad02b028
NadaAmrr <nada.amr1267@gmail.com>
2023-11-26 13:38:22 +0200

    error handling

--- src/utils/errorHandling.js
+++ src/utils/errorHandling.js
@@ -14,7 +14,7 @@ export const globalErrorHandling = (error, req, res, next) => {
   return res
     .status(error.status || 400)
     .json({
-      message: "G Error",
+      message: "Error",
       messageError: error.message,
     //   stack: error.stack,
     });
